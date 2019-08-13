f86cc8e2eb142f760c5a4933acb9f8c13e9ec090
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 17:59:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+  is_prime = false
+ array = (1..10).to_a
+ array_divisible = []
+
+  if num < 0 
+    false
+  else
+    if num.is_a? Integer 
+      num = 10
+     array_divisible =  array.collect do |el| 
+                                          num % el
+                                      end
+     p num
+     p array_divisible
+    end 
+    
+  end
+  is_prime
+end
