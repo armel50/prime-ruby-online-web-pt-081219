352291e8760d5d6f2827a4ae9335dfed96908ed2
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 18:28:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,15 +7,11 @@ def prime?(num)
     
     array = (1..num ).to_a 
       array_divisible = []
-      
-  #  if (num.is_a? Integer) 
 
        array.each { |el|  array_divisible << el if  num % el == 0}
        
     array_divisible.length > 2 ? is_prime = false : is_prime = true
 
-   
-    
   end
   is_prime
 end
